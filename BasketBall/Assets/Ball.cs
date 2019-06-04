567b3dc3acae90af96eea31b33a0c89f20e5b463
klstewart95 <klstewart95@gmail.com>
2019-06-04 19:46:10 -0400

    Game Upload
    Full Game Upload

---
+++ BasketBall/Assets/Ball.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float Velocity;
+    float Vx;
+    float Vy;
+
+    public float Theta;
+
+    Vector3 BallMovement = Vector3.zero;
+
+    bool ShotBall = false;
+
+	// Use this for initialization
+	void Start () {
+        
+            
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!ShotBall)
+            {
+                LaunchBall();
+                ShotBall = true;
+
+                Destroy(gameObject, 5);
+            }
+        }
+
+        if (ShotBall)
+        {
+            getVelocity();
+        }
+    }
+
+    public void LaunchBall()
+    {
+        Vx = Velocity * (Mathf.Cos(Theta * Mathf.Deg2Rad));
+        Vy = Velocity * (Mathf.Sin(Theta * Mathf.Deg2Rad));
+
+        BallMovement.z = Vx;
+        BallMovement.y = Vy;
+    }
+
+    void getVelocity()
+    {
+        BallMovement.y = BallMovement.y - (.98f);
+        transform.Translate(BallMovement * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        BallMovement.z = (BallMovement.z * 0.5f) * -1;
+
+        if(other.tag == "Hoop")
+        {
+            Debug.LogError("Scored");
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ BasketBall/Assets/BasketballManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketballManager : MonoBehaviour {
+
+    public GameObject Basketball;
+    public Transform Spawn;
+    Vector3 SpawnPoint;
+
+    private void Start()
+    {
+        SpawnPoint = Spawn.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Instantiate(Basketball, SpawnPoint, Quaternion.identity);
+        }
+    }
+}
